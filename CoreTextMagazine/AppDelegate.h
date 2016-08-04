59597f920e562cf1105a86dc34d67ca4d2d81ae8
Swarup_Pattnaik <pattnaik.swarup@gmail.com>
2016-08-04 08:25:40 +0530

    Initial Commit

---
+++ CoreTextMagazine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextMagazine
+//
+//  Created by Swarup_Pattnaik on 04/08/16.
+//  Copyright © 2016 Swarup_Pattnaik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreTextMagazine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreTextMagazine
+//
+//  Created by Swarup_Pattnaik on 04/08/16.
+//  Copyright © 2016 Swarup_Pattnaik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
